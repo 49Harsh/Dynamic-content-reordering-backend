4e9ab0295f578efa6a1a12c21f9cf1cb694edf8c
Harsh-Portfolio <49harshyadav@gmail.com>
2024-07-08 23:12:44 +0530

    created all model

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
+
+
+const articleRoutes = require('./routes/articleRoutes');
+app.use('/api/articles', articleRoutes);
+
+const PORT = process.env.PORT || 5000 ;
+app.listen(PORT, () =>
+    console.log(`Server started on port ${PORT}`));
